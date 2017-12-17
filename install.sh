12462ac12c4ec14ed148772c9f1acc086582a43d
root <root@kali>
2017-12-17 14:43:25 -0500

    fixed typo

---
+++ install.sh
@@ -0,0 +1,15 @@
+# Installation script...
+
+# add some vim folders
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/backups
+mkdir -p ~/.vim/undo
+
+
+
+# link plug.vim seems failing, copy that
+cp $(pwd)/vim/autoload/plug.vim ~/.vim/autoload
+
+echo "Change the xfce-default terminal Settings by hand!...."
+
+
